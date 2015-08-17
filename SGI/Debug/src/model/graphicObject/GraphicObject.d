68049734dc162b2f8d0df6009dc3048dde873d27
Arthur Erpen <arthurerpen@gmail.com>
2015-08-17 20:02:34 -0300

    arrumado hierarquia e adicionado gitnore

--- SGI/Debug/src/model/graphicObject/GraphicObject.d
+++
@@ -1,5 +0,0 @@
-src/model/graphicObject/GraphicObject.d: \
- ../src/model/graphicObject/GraphicObject.cpp \
- ../src/model/graphicObject/GraphicObject.h
-
-../src/model/graphicObject/GraphicObject.h:
--- SGI/Debug/src/model/graphicObject/Point.d
+++
@@ -1,7 +0,0 @@
-src/model/graphicObject/Point.d: ../src/model/graphicObject/Point.cpp \
- ../src/model/graphicObject/Point.h \
- ../src/model/graphicObject/GraphicObject.h
-
-../src/model/graphicObject/Point.h:
-
-../src/model/graphicObject/GraphicObject.h:
